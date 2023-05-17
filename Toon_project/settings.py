422578a7db270294d40cc11601c2664fefe45c8e
yeonmi <wkdus885@naver.com>
2023-05-17 21:57:51 +0900

    host

--- Toon_project/settings.py
+++ Toon_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3e&s9ht4x&iz5@%io@wwdps84))gi600^q#jjfq)7d1xar**%a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]
 
 
 # Application definition
