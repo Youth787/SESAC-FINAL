2283dc942b45feef11007f47f2739da8ae557ad9
yeonmi <wkdus885@naver.com>
2023-05-18 17:29:17 +0900

    static

--- Toon_project/settings.py
+++ Toon_project/settings.py
@@ -131,7 +131,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
